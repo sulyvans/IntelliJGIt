b7c540be2f3a84151ce10ad848a369848cb84de3
Tateenha <147776407+Tateenha@users.noreply.github.com>
2023-10-17 15:32:37 +0200

    Archivos añadidos por Lilith Simon

---
+++ src/Ejercicio1.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Bienvenido a entornos de desarrollo donde vas a aprender a programar en Java");
+        System.out.println(3+4);
+        }
+    }
